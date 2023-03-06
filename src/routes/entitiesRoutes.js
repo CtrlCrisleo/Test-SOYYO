d8ce6c1481d2fa02a94d6fa13c59d1c224ea78d7
Cristhian <cristhian.lcortes@gmail.com>
2023-03-05 19:46:19 -0500

    add comments

--- src/routes/entitiesRoutes.js
+++ src/routes/entitiesRoutes.js
@@ -3,6 +3,7 @@ import entitiesController from '../controllers/entitiesController.js';
 
 const router = new Router();
 
+/*GET Method*/
 router.get('/:startId/:endId', entitiesController.getEntities);
 
 export default router;
